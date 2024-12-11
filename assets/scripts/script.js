0754728ba114b1b1b3253a7d21a03f38e3f5808f
Anderson <andslipknoty@gmail.com>
2024-12-11 18:04:17 -0300

    active funciona

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,9 @@
 const startBtn = document.querySelector('.start-btn');
-const ruleForm = document.querySelector('.form-container.active')
+const ruleForm = document.querySelector('.form-container')
 const quizBox = document.querySelector('.quiz-box')
 
 startBtn.onclick = () => {
+    ruleForm.classList.add('active');
     quizBox.classList.add('active');
-    ruleForm.classList.remove('active');
+    
 }
