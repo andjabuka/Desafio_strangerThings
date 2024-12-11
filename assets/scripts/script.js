e3062ca478fff1e8903f6f8701da1fb43ad820d5
Anderson <andslipknoty@gmail.com>
2024-12-11 18:42:54 -0300

    commit5

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,8 +2,83 @@ const startBtn = document.querySelector('.start-btn');
 const ruleForm = document.querySelector('.form-container')
 const quizBox = document.querySelector('.quiz-box')
 
+
+
 startBtn.onclick = () => {
     ruleForm.classList.add('active');
     quizBox.classList.add('active');
     
+}
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if (questionCount <questions.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+
+        nextBtn.classList.add('active');
+    }
+    else {
+
+        showResultBox();
+    }
+}
+
+const optionList = document.querySelector('.option-list');
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+                     <div class="option"><span>${questions[index].options[1]}</span></div>
+                     <div class="option"><span>${questions[index].options[2]}</span></div>
+                     <div class="option"><span>${questions[index].options[3]}</span></div>`;   
+    
+optionList.innerHTML = optionTag;
+
+const option = document.querySelectorAll('.option');
+for (let i=0; i < option.length; i++) {
+    option[i].setAttribute('onclick', 'optionSelected(this)')
+}
+
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+    }
+    else {
+        answer.classList.add('incorrect');
+
+        for (let i = 0; i < allOptions; i++) {
+            if (optionList.children[i].textContent == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+
+    }
+
+    //if user has selected, disabled all options
+    for (let i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector(('.question-total'))
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
 }
