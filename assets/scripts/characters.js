d54737f0d8a67433fc251fbd6577821d47593e08
Anderson <andslipknoty@gmail.com>
2024-12-13 12:41:20 -0300

    commit6

--- assets/scripts/characters.js
+++ assets/scripts/characters.js
@@ -7,47 +7,47 @@ const characterDescriptions = {
     Dustin: {
         title: "Você seria, Dustin Henderson em Stranger Things",
         description: "Parabéns! Você é Leal, engraçado e incrivelmente inteligente como o Dustin, que é o coração do grupo de amigos em Stranger Things. Com seu senso de humor contagiante e curiosidade científica, você enfrenta os desafios com criatividade e otimismo.",
-        image: "path/to/dustin-image.jpg"
+        image: "assets/images/Dustin.jpg"
     },
-    // Adicione os outros personagens aqui
+
     Mike: {
         title: "Você seria, Mike em Stranger Things",
         description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Mike.webp"
     },
     Will: {
         title: "Você seria, Will em Stranger Things",
         description: "Você é sensível, leal e criativo, com uma imaginação incrível que transforma qualquer situação em algo especial. Sua conexão com os amigos é profunda, e você valoriza a amizade acima de tudo, mesmo enfrentando desafios complexos.",    
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Will.webp"
     },
     Lucas: {
         title: "Você seria, Lucas em Stranger Things",
         description: "Você é corajoso, pragmático e sempre mantém os pés no chão, equilibrando lógica e emoção. Com um coração leal e um senso de humor afiado, você não tem medo de questionar o grupo, mas está sempre lá quando importa.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Lucas.webp"
     },
     Max: {
         title: "Você seria, Max em Stranger Things",
         description: "Você é independente, ousado e cheio de atitude, com um espírito aventureiro que inspira aqueles ao seu redor. Mesmo enfrentando desafios pessoais, você é leal aos seus amigos e está sempre disposto a lutar pelo que acredita.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Max.webp"
     },
     Jim: {
         title: "Você seria, Jim em Stranger Things",
         description: "Você é protetor, resiliente e tem um grande coração por trás de sua postura firme. Sempre disposto a enfrentar qualquer perigo para proteger aqueles que ama, você combina força e sensibilidade de forma única, sendo o alicerce de quem está ao seu redor.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Jim.webp"
     },
     Joyce: {
         title: "Você seria, Joyce em Stranger Things",
         description: "Você é determinado, empático e incansável quando se trata de proteger as pessoas que ama. Sua intuição e força emocional são impressionantes, e você está sempre disposto a enfrentar qualquer obstáculo, mesmo que pareça impossível.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Joice.webp"
     },
     Steve: {
         title: "Você seria, Steve em Stranger Things",
         description: "Você é carismático, protetor e surpreendentemente dedicado aos seus amigos. Por trás de sua atitude descontraída, existe um verdadeiro herói que está sempre pronto para entrar em ação e colocar os outros em primeiro lugar, mesmo nas situações mais perigosas.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Steve.webp"
     },
     Nancy: {
         title: "Você seria, Nancy em Stranger Things",
         description: "Você é inteligente, destemido e determinado a lutar pela verdade, mesmo quando enfrenta grandes desafios. Com sua coragem e senso de justiça, você inspira aqueles ao seu redor e não tem medo de tomar a liderança para proteger o que é certo.",
-        image: "path/to/mike-image.jpg"
+        image: "assets/images/Steve.webp"
     },
 };    
